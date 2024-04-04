39bab0e714f2fca9196bcbf639cb334d0ed168ac
Shubh zala <98080018+ShubhZala@users.noreply.github.com>
2024-04-04 12:45:54 -0500

    Add files via upload

---
+++ Capstone_Chatbot.py
@@ -0,0 +1,19 @@
+import openai
+import gradio
+openai.api_key = "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"
+
+messages = [{"role": "system", "content": "You are a Medical experts that specializes in helping patient to get cure from Tuberculosis"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": "Patient"})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Tuberculosis Chatbot")
+
+demo.launch(share=True)
